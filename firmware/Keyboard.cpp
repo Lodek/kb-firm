4273b35d30059c833143e97d2cbb76111355e5bd
Lodek <bruno.dsly@gmail.com>
2017-08-25 12:53:27 -0300

    Fixed whitespace/indenting Commented code Ready for the other behavior functions

--- firmware/Keyboard.cpp
+++ firmware/Keyboard.cpp
@@ -25,60 +25,47 @@ long mapping[NUMLAYERS][NUM_KEYS] =
 	{0x003a, 0x003b, 0x003c, 0x003d, 0x003e, 0x003f, 0x0040, 0x0041, 0x0042, 0x0043, 0x0044, 0x002a, 0x002a, 0x0039, 0x007f, 0x0081, 0x0080, 0x0074, 0x0078, 0x0050, 0x0051, 0x0052, 0x004f, 0x0048, 0x0045, 0x0028, 0x0200, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x004a, 0x004e, 0x004b, 0x004d, 0x0046, 0x2000, 0x0028, 0x0100, 0x0800, 0x0400, 0x0000, 0x010000, 0x0028, 0x0028, 0x020000, 0x0000, 0x0000, 0x0000, 0x1000, 0x002c},
 };
 
-
-
-//Startup Functions
-
-
+// Initialize functions
 void init_mappings()
 {
-    for (int i = 0; i < NUM_KEYS; i++)
+	for (int i = 0; i < NUM_KEYS; i++)
     {
-        matrix[i].important = 0;
-        matrix[i].on_buffer = 0;
-        matrix[i].state = 0;
-        for (int j = 0; j < NUMLAYERS; j++)
-        {
+		 matrix[i].important = 0;
+         matrix[i].on_buffer = 0;
+         matrix[i].state = 0;
+         for (int j = 0; j < NUMLAYERS; j++)
+         {
             matrix[i].data[j].behavior = normal;
-            matrix[i].data[j].keycode = mapping[j][i];
-        }
+			matrix[i].data[j].keycode = mapping[j][i];
+         }
     }
     return;
 }
 
 
 void _begin()
-/*
-begins commns serial communication
-no longer needed for pro micro, still used for uno and mega 
-*/
 {
+	/* begins commns serial communication
+	no longer needed for pro micro, still used for uno and mega */
     Serial.end();
     Serial.begin(9600);
 }
 
 
 void startup_routine()
-/*
-initialize microcontroller
-sets rows as outputs and colls as inputs
-begins serial communication
-*/
 {
+	/* initialize microcontroller
+	sets rows as outputs and colls as inputs
+	begins serial communication */
     for (int i = 0; i < NUM_ROW; i++) //sets row pins as OUT
     {
         pinMode(row_pins[i], OUTPUT);
         digitalWrite(row_pins[i], LOW);
     }
-
     for (int i = 0; i < NUM_COLL; i++) //sets colls pins as IN
-    {
         pinMode(col_pins[i], INPUT);
-    }
-
     _begin();
     init_mappings();
-
     return;
 }
 
@@ -88,21 +75,16 @@ begins serial communication
 
 
 void matrix_scan()
-/*
-uses col_pins & row_pins
-loops through each row; sets it to high, calls digital read for each coll_pin member and assigns it to the matching key object
-writes result of digital read to the matching key on the matrix array
-resets current row to low
-repeats for next row
-*/
 {
+	/* uses col_pins & row_pins
+	loops through each row; sets it to high, calls digital read for each coll_pin member and assigns it to the matching key object
+	writes result of digital read to the matching key on the matrix array
+	resets current row to low
+	repeats for next row */
     for (int i = 0; i < NUM_ROW; i++)
     {
         digitalWrite(row_pins[i], HIGH);
-        
-		for (int j = 0; j < NUM_COLL; j++)
-	    matrix[i * NUM_COLL + j].state = digitalRead(col_pins[j]);
-       
+		for (int j = 0; j < NUM_COLL; j++)  matrix[i*NUM_COLL+j].state = digitalRead(col_pins[j]);
 		digitalWrite(row_pins[i], LOW);
     }
     return;
@@ -110,54 +92,48 @@ repeats for next row
 
 
 void matrix_iterator()
-//calls the key handler for each element of the matrix array
 {
+	//calls the key handler for each element of the matrix array
     for (int i = 0; i < NUM_COLL * NUM_ROW; i++)
     {
 		key_handler(&matrix[i]);
     }
-
     return;
 }
 
 
 void key_handler(key *current_key)
-//key handler receives a key struct object
-//it first checks whether the key needs to be handled
-//then it checks the key behavior and sends calls the appropriate handler function for that key type
 {
-    if (current_key->state == 0 && current_key->important == 0) return;
+	/* key handler receives a key struct object
+	it first checks whether the key needs to be handled
+	then it checks the key behavior and sends calls the appropriate handler function for that key type */
+    (current_key->state == 0 && current_key->important == 0) return; // key is neither pressed nor important
 
-    if (current_key->data[layervar_translator(layervar)].behavior == normal) behavior_normal(current_key);
-  
+    if (current_key->data[layervar_translator(layervar)].behavior == normal) behavior_normal(current_key); // normal key
     return ;
 }
 
 
 void write_buffer()
-//Sends the USB report to the host, the data element of the report is out_buffer which has length 8
 {
+	//Sends the USB report to the host, the data element of the report is out_buffer which has length 8
     HID_SendReport(2, out_buffer, 8);
     return;
 }
 
 
-
 //Behavior Functions
-
 
 void behavior_normal (key *current_key)
 {
     long keycode;
   
     if (current_key->state == 1 && current_key->important == 1) return; //key was and still is pressed, nothig to be done
-  
+
     if (current_key->state == 0 && current_key->important == 1) //is called when key has been released but its info is still on buffer and/or layervar
     {
         keycode = current_key->on_buffer;
-
         remove_from_buffer(keycode);
-
         current_key->on_buffer = -1;
         current_key->important = 0;
     }
@@ -165,87 +141,74 @@ void behavior_normal (key *current_key)
     else if (current_key->state == 1 && current_key->important == 0) //key pressed and not on buffer
     {
         keycode = current_key->data[layervar_translator(layervar)].keycode;
-  
         current_key->on_buffer = add_to_buffer(keycode); //calls add to buffer and writes the return to on_buffer variable
-  
+
         if (current_key->on_buffer != -1)
         {
 		    //add to buffer returns -1 if buffer is full. if buffer is full important remains 0 and key is proccessed next cycle
 	        //sets the 2nd byte of on buffer with HID Modifiers used and the 3rd byte with layer value. Info needed for effective removal from buffer
             current_key->on_buffer = ((keycode & 0xFFFF00) | current_key->on_buffer);
-  
             current_key->important = 1; //important=1 is important....
         }
     }
-  return;
+    return;
 }
-
 
 
 //Utility functions
 
-
 int add_to_buffer(long key)
-/*writes a key to the first empty spot found on the buffer
-returns k if added, returns -1 if the buffer was full
-ors modifiers to append them
-ors layers with layervar to append layer
-*/
 {
-	int result = -1;
-  
-    //search for empty spot on output buffer and writes to it
-    for (int k = 2; k < 8; k++)
-    {
-        if (out_buffer[k] == 0)
-        {
-          out_buffer[k] = (key&0x0000FF);
-          result = k;
-          k = 9;
-    
-        }
-    }
-  
-    layervar = (key >> 16) | layervar; //ORs layer byte value of keycode to layervar
-  
-    //OR's modifiers byte to add
-    //bit shifts 8 to the right (position it on the first byte) AND with 0x0000FF to get rid of layer byte
-    //finaly OR with out_buffer to append new modifier
-    out_buffer[0] = out_buffer[0] | ((key >> 8) & 0x0000FF);
+	/* Receives a key and does 3 operations. Returns the index of out_buffer where the HID Keycode was written to. If buffer is full, returns -1. 
+	a - isolates and adds HID Keycode part of key to out_buffer (first byte of key, bitshifts and ANDs to isolate it).
+	b - isolates and appends (ORs) HID modifier to out_buffer[0] (second byte of key).
+	c - isolates and appends (ORs) layer byte of key to layervar (third byte of key) */
+	int result = -1; 
+	//In case key's 1st byte is 0, return 1. 1 is the reserved byte of the HID descriptor
+	//Needed since otherwise 2 keys might point to the same index of out_buffer. Then releasing a key would release both and lead to an awkward situation
+	if(key&0x0000FF==0) result=1;
+	else
+	{
+	    for (int k = 2; k < 8; k++) //loops through out buffer looking for the first empty spot
+	    {
+	        if (out_buffer[k] == 0)
+	        {
+				out_buffer[k] = (key&0x0000FF); //a
+				result = k;
+				k = 9;
+			}
+	    }
+	}
+    out_buffer[0] = out_buffer[0] | ((key >> 8) & 0x0000FF); //b 
+    layervar = (key >> 16) | layervar; //c 
     return result;
 }
 
 
 int remove_from_buffer(long key)
-//removes the received key from the buffer
 {
-    uint8_t mods, bufferpos;
-  
-    bufferpos = (key & 0x0000FF); //isolates first byte of key to get buffer index of the given key
-    mods = ((key >> 8) & 0x0000FF); //isolates second byte of key to get modifiers written by that key
-    layervar = (key >> 16)^layervar; //isolates the layer byte from keycode and XOR with layer var to remove it
-  
-    out_buffer[bufferpos] = 0; //erases out buffer using isolated buffer pos
-  
-    //XOR's modifiers byte to subtract new ones
-    out_buffer[0] = out_buffer[0] ^ mods;
+	/*removes the received key from the buffer and removes its value from layer var
+	3 removal steps
+	a - Removes key from buffer. Isolates first byte of key which contains the position of that key on out_buffer
+	b - Removes modfier from buffer. Isolates second byte of key. That byte contais HID Mod code. Uses the data and XOR's it with the modifier byte of out_buffer
+	c - Removes layer value from layervar. Isolates third byte of key which has layer value. Similarly, XOR's it with layervar to subtract its value */
+    out_buffer[(key & 0x0000FF)] = 0; //a
+	out_buffer[0] = out_buffer[0] ^ ((key >> 8) & 0x0000FF);//b
+    layervar = (key >> 16)^layervar;//c
     return 0;
 }
 
 
 void flush()
-//loops through out_buffer zeroing all elements of the array
 {
+	//loops through out_buffer zeroing all elements of the array
     for (int i = 0; i < 8; i++) out_buffer[i] = 0;
     return;
 }
 
 
 int layervar_translator(int layer)
-//function that matches the index of the corresponding VALUE of a layer (1,2,4,8...)
 {
-	for (int i = 0; i < NUMLAYERS ; i++)
-    {
-    	if (layers[i] == layer) return i;
-    }
+	//function that returns the index for the corresponding VALUE of a layer (1,2,4,8...)
+    for (int i = 0; i < NUMLAYERS ; i++) if (layers[i] == layer) return i;
 }
--- firmware/Keyboard.h
+++ firmware/Keyboard.h
@@ -1,58 +1,64 @@
+#include <Arduino.h>
 #define NUM_COLL 13
 #define NUM_ROW 4
 #define NUM_PINS NUM_ROW+NUM_COLL
 #define NUM_KEYS NUM_COLL*NUM_ROW
 #define NUMLAYERS 3
 #define LAYERVALUE 0,1,2
-#include <Arduino.h>
+
 
 //possible behaviors of a key
-typedef enum
-{normal, hold, doubletap, clear, macro, dth, dead} behavior_enum; 
+typedef enum {normal, hold, doubletap, clear, macro, dth, dead} behavior_enum; 
 
-//struct that stores the information of each key for each layer
+
 typedef struct
 {
-	//defines the behavior of said key
-    behavior_enum behavior;
+	//struct that stores the information of each key for each layer
+    behavior_enum behavior;	//defines the behavior of said key
 
-    //contains either a pointer to a struct of type hold, doubletap...
-	//or carries the keycode on the format 0xLLMMKK
-	//where LL the byte for layer selection
-	//MM is the byte for HID modifiers
-	//KK is the byte for HID key  
+    /*contains either a pointer to a struct of type hold, doubletap...
+	or carries the keycode on the format 0xLLMMKK
+	where LL the byte for layer selection
+	MM is the byte for HID modifiers
+	KK is the byte for HID key  */
     long keycode; 
 } key_data;
 
-//actual definition of a key
-//contains state variables and an array of type key_data
-//the array carries the data/behavior of the key for each layer defined by the user
+
 typedef struct
 {
+	/* actual definition of a key
+	contains state variables and an array of type key_data
+	the array carries the data/behavior of the key for each layer defined by the user */
 	int state; // state of the key (1/0) from the time the matrix was scanned
     int important; //ON/OFF is not enough to track a key, this allows the code to decide whether this key requires attention or not
+
+	/* on buffer has value of -1 if the key is not on the buffer/is not being used for layer
+	otherwise it follows a similar format to the keycode variable: 0xLLMMBB
+	LL is the layer byte,  MM HID mod byte
+	BB carries the position of that key on the out_buffer array */
     long on_buffer;
-	// on buffer has value of -1 if the key is not on the buffer/is not being used for layer
-	//otherwise it follows a similar format to the keycode variable
-	//0xLLMMBB
-	//LL is the layer byte
-	// MM HID mod byte
-	// BB is the difference, it instead of containing the HID keycode, it carries the position of that key on the out_buffer array
     
 	key_data data[NUMLAYERS]; //array with the data of the behavior/function of this key for each layer
   
 } key;
 
+//Startup functions
+void init_mappings();//maps the mapping array to the key structs
+void _begin(); //init serial comms
 void startup_routine(); //sets up the pins in the arduino
-void _begin(); //serial?
-void init_mappings();
-void matrix_scan();
-void matrix_iterator();
-void key_handler(key* current_key);
-void behavior_normal(key* current_key);
-void flush();//zero out buffer
+
+//Loop functions
+void matrix_scan();//performs the scan of the matrix and writes 1/0 to each key
+void matrix_iterator();//iterates through each key and calls the handler function
+void key_handler(key* current_key);//calls the appropriate handler for that key
 void write_buffer();//sends buffer to host
 
-int layervar_translator(int layer);
-int add_to_buffer(long key);
-int remove_from_buffer(long key);
+//Behavior functions
+void behavior_normal(key* current_key); //handles "normal" keys
+
+//Utility functions
+int add_to_buffer(long key);//receives a standard key, parses it and add to buffer
+int remove_from_buffer(long key);//receives std key, parses it and erases it from buffer
+void flush();//zero out buffer
+int layervar_translator(int layer); //function that returns the index for the corresponding VALUE of a layer.
